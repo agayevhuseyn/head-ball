44c79a42ea4eb2672a740634acae47cdd93ac683
Huseyn Aghayev <huseyn@seallang.org>
2025-10-25 13:50:10 +0400

    update fps

--- src/config.h
+++ src/config.h
@@ -7,7 +7,7 @@
 #define WIDTH  1280
 #define HEIGHT 720
 #define TITLE  "Head Ball"
-#define FPS 60
+#define FPS 120
 #define GRAVITY 2200
 #define SCALE 4
 #define GROUND (HEIGHT - 20 /* ground in sprite */ * SCALE)
