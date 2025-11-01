2b0158ef21f716f6e5318a5780708f1b15ba3a31
Huseyn Aghayev <huseyn@seallang.org>
2025-11-01 09:50:27 +0400

    statically build for linux and windows

--- build.sh
+++ build.sh
@@ -8,15 +8,20 @@ SRC_DIR="src"
 OBJ_DIR="objects"
 RAYLIB_LIB="raylib-libs"
 STATIC=false
+PLATFORM="linux"
 
 usage() {
     echo "unknown flag: $1"
-    echo "available: -s|--static"
+    echo "available: -s|--static --windows"
 }
 
 for i in "$@"; do
     case $i in
         -s|--static)
+            STATIC=true
+            ;;
+        --windows)
+            PLATFORM="win"
             STATIC=true
             ;;
         *)
@@ -27,19 +32,38 @@ for i in "$@"; do
 done
 
 if $STATIC; then
-    FLAGS="$FLAGS $RAYLIB_LIB/linux/libraylib.a
-           -I $RAYLIB_LIB/linux/include"
+    LINKED="$RAYLIB_LIB/$PLATFORM/libraylib.a"
+    echo "Statically linking with $LINKED ..."
+    FLAGS="$FLAGS $LINKED"
+fi
+if [[ $PLATFORM == "linux" ]]; then
+    echo "Compiling for Linux..."
+    if ! $STATIC; then
+        FLAGS="$FLAGS -ldl -lX11 -lraylib"
+    fi
 else
-    FLAGS="$FLAGS -lraylib"
+    echo "Compiling for Windows..."
+    FLAGS="$FLAGS -lgdi32 -lwinmm"
+fi
+
+if [[ $PLATFORM == "win" ]]; then
+    CC="x86_64-w64-mingw32-gcc"
+    OUT="$OUT.exe"
+    if ! command -v $CC &>/dev/null; then
+        echo "$CC compiler is not found on your machine"
+        exit 1
+    fi
 fi
 
 mkdir -p $OBJ_DIR
 
 for file in $SRC_DIR/*.c; do
-	$CC -c $file -I $SRC_DIR -o "$OBJ_DIR/$(basename $file .c).o"
+	$CC -c $file -I $SRC_DIR -o "$OBJ_DIR/$(basename $file .c).o" -I $RAYLIB_LIB/$PLATFORM/include
 done
 
 $CC $OBJ_DIR/*.o -o $OUT $FLAGS
 
+echo "Removing object files..."
 rm -r $OBJ_DIR
-./$OUT
+
+echo "Executable is ready as $OUT"
